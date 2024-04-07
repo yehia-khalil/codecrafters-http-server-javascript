4142efdd16899506fdccaf96111f1e9fecb02536
Yehia Khalil <yehiakhalilykk@gmail.com>
2024-04-07 22:51:32 +0200

    test get

--- app/main.js
+++ app/main.js
@@ -1,14 +1,25 @@
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+    socket.on('connect', () => {
+        console.log("connect")
+    })
+    socket.on("ready", () => {
+        console.log("ready")
+    })
+    socket.on('data', () => {
+        console.log("HTTP/1.1 200 OK\r\n\r\n")
+        socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        socket.end();
+        server.close();
+    })
+    socket.on("close", () => {
+        socket.end();
+        server.close();
+    });
 });
 
 server.listen(4221, "localhost");
